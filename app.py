e876e6073e7a2d6911161d2a55fb231b199a776b
Karina Ionkina <kionkina@stuy.edu>
2017-10-02 13:13:15 -0400

    added name/req/greeting

--- app.py
+++ app.py
@@ -6,16 +6,20 @@ app = Flask(__name__) #create instance of class
 @app.route("/")
 def hello():
     print app
-    print "request.headers"
+    print "***request.headers***"
     print request.headers
-    print "request.method"
-    print request.method
-    print "request.args"
+    print "***request.method***"
+    r_method = request.method
+    print "***request.args***"
     print request.args
-    print "request.form"
+    print "***request.args[firstname]***"
+    try:
+        their_name = request.args['firstname']
+    except:
+        print "cannot store name"
+    print "***request.form***"
     print request.form
-    return  render_template('test.html') 
-
+    return  render_template('test.html', their_name = their_name, r_method = r_method)
 
 if __name__=="__main__":
     app.debug = True
